071b3dd97b1401518f889b922bb028180dfa934c
Joseph Toh Djojo <joseph.tohdjojo@gmail.com>
2018-01-03 17:15:02 -0500

    got input container to work

--- src/index.js
+++ src/index.js
@@ -1,9 +1,45 @@
-const App = props => {
+const InputContainer = (props) => {
   return (
-    <div>
-      Hello world!
-    </div>
+    <form onSubmit={ props.handleSubmit }>
+      <input type="text" onChange={ props.handleChange }/>
+      <input type="submit" value="Submit"/>
+    </form>
   )
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      inputText: '',
+    }
+
+    this.updateInputText = this.updateInputText.bind(this)
+    this.alertInputText = this.alertInputText.bind(this)
+  }
+
+  updateInputText(e) {
+    e.preventDefault()
+
+    this.setState({
+      inputText: e.target.value,
+    })
+  }
+
+  alertInputText(e) {
+    e.preventDefault()
+
+    alert(this.state.inputText)
+  }
+
+  render() {
+    return (
+      <div>
+        <InputContainer handleChange={ this.updateInputText } handleSubmit={ this.alertInputText }/>
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(<App/>, document.getElementById('root'))
